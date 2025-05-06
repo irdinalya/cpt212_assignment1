6737368a790eb7fa9ca0268c14039adf0c3ce35c
irdinalya <irdinalya3@gmail.com>
2025-05-06 12:43:19 +0800

    2

--- src/CustomSortIntegers.java
+++ src/CustomSortIntegers.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class CustomSortIntegers {
     public static void main(String[] args) {
@@ -16,22 +16,7 @@ public class CustomSortIntegers {
         System.out.println("\nInitial array:");
         printArray(numbers);
 
-        // Iteration: Sorting pass by pass (selection sort)
-        for (int i = 0; i < size - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < size; j++) {
-                if (numbers[j] < numbers[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            // Swap the found minimum element with the first element
-            int temp = numbers[minIndex];
-            numbers[minIndex] = numbers[i];
-            numbers[i] = temp;
-
-            System.out.println("After pass " + (i + 1) + ":");
-            printArray(numbers);
-        }
+        radixSort(numbers);
 
         System.out.println("\nFinal sorted array:");
         printArray(numbers);
@@ -45,5 +30,60 @@ public class CustomSortIntegers {
             System.out.print(num + " ");
         }
         System.out.println();
+    }
+
+    // Get maximum value in array
+    public static int getMax(int[] array) {
+        int max = array[0];
+        for (int num : array) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    // Radix sort
+    public static void radixSort(int[] array) {
+        int max = getMax(array);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSortByDigit(array, exp);
+            int pass = (int)(Math.log10(exp)) + 1;
+            System.out.println("After Pass " + pass + ":");
+
+            printArray(array);
+        }
+    }
+
+    // Counting sort for a specific digit
+    public static void countingSortByDigit(int[] array, int exp) {
+        int n = array.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        // Count occurrences of digits
+        for (int i = 0; i < n; i++) {
+            count[(array[i] / exp) % 10]++;
+        }
+
+        // Change count[i] so that count[i] contains the actual
+        // position of this digit in output[]
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Build the output array
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = (array[i] / exp) % 10;
+            output[count[digit] - 1] = array[i];
+            count[digit]--;
+        }
+
+        // Copy the output array to array[], so that array[] now
+        // contains sorted numbers according to the current digit
+        for (int i = 0; i < n; i++) {
+            array[i] = output[i];
+        }
     }
 }
